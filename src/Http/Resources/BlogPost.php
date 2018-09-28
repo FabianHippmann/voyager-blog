102a4b29ff375ff8adea2e25b2f409c84d55812c
FabianHippmann <fabian.hippmann@moonshiner.at>
2018-09-29 01:21:26 +0200

    add image to json resource

--- src/Http/Resources/BlogPost.php
+++ src/Http/Resources/BlogPost.php
@@ -2,6 +2,7 @@
 
 namespace Pvtl\VoyagerBlog\Http\Resources;
 
+use TCG\Voyager\Facades\Voyager;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BlogPost extends JsonResource
@@ -19,6 +20,7 @@ class BlogPost extends JsonResource
             'body' => $this->body,
             'created_at' => $this->created_at->format('d.m.Y'),
             'slug' => $this->slug,
+            'image' => Voyager::image($this->image),
             'meta_description' => $this->meta_description,
             'title' => $this->title
         ];
